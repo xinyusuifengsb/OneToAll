578fbba4288ae9c47b58df0dd83431952dee72eb
xinyusuifengsb <347697094@qq.com>
2024-05-09 14:06:18 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace 一键启动
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.删除ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(5, 5);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(218, 199);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.删除ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 26);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // 删除ToolStripMenuItem
+            // 
+            this.删除ToolStripMenuItem.Name = "删除ToolStripMenuItem";
+            this.删除ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.删除ToolStripMenuItem.Text = "删除";
+            this.删除ToolStripMenuItem.Click += new System.EventHandler(this.删除ToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(5, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(218, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "添加程序";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(5, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(218, 48);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "一键启动";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(232, 320);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "多开一键启动";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 删除ToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace 一键启动
+{
+    public partial class Form1 : Form
+    {
+        private ImageList imageList = new ImageList();
+        
+        public Form1()
+        {
+            
+            InitializeComponent();
+            LoadSoftwarePaths(); // 主窗体构造函数，当窗体创建时会调用
+            listView1.SmallImageList = imageList; // 关联ImageList
+                                                  
+            this.Load += new EventHandler(Form1_Load);// 在窗体加载时检查参数
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Executable files (*.exe)|*.exe|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // 添加软件路径到列表
+                string filePath = openFileDialog.FileName;
+                AddSoftwareToList(filePath);
+
+                // 保存软件路径到本地设置或文件
+                SaveSoftwarePaths();
+            }
+        }
+
+        private void AddSoftwareToList(string path)
+        {
+            // 获取文件的图标
+            Icon icon = Icon.ExtractAssociatedIcon(path);
+
+            // 添加图标到ImageList
+            imageList.Images.Add(path, icon.ToBitmap());
+
+            // 添加项目到ListView
+            ListViewItem item = new ListViewItem(Path.GetFileNameWithoutExtension(path), imageList.Images.IndexOfKey(path));
+            item.Tag = path; // 用Tag属性保存完整路径
+            listView1.Items.Add(item);
+        }
+
+        private void SaveSoftwarePaths()
+        {
+            // 在此处实现具体的保存逻辑，例如使用Settings或写入一个本地文件
+            using (StreamWriter sw = new StreamWriter("path.txt"))
+            {
+                foreach (ListViewItem item in listView1.Items)
+                {
+                    sw.WriteLine(item.Tag.ToString());
+                }
+            }
+        }
+
+        private void LoadSoftwarePaths()
+        {
+            if (File.Exists("path.txt"))
+            {
+                using (StreamReader sr = new StreamReader("path.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        AddSoftwareToList(line);
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] args = Environment.GetCommandLineArgs(); // 获取所有命令行参数
+            if (args.Contains("-s"))
+            {
+                // 如果是静默模式，最小化窗口并从任务栏隐藏
+                this.WindowState = FormWindowState.Minimized;
+                this.ShowInTaskbar = false;
+
+                // 自动触发startAllButton的点击事件
+                button2_Click(this, EventArgs.Empty);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("path.txt"))
+            {
+                MessageBox.Show("没有找到软件路径文件！");
+                return;
+            }
+
+            using (StreamReader sr = new StreamReader("path.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    try
+                    {
+                        Process.Start(line);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"无法启动指定路径的软件：{line}。错误：{ex.Message}");
+                    }
+                }
+            }
+
+        }
+
+        private void 删除ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                if (MessageBox.Show("确定要删除选中的项目吗？", "删除确认", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    // 如果用户确认删除，则移除选中项
+                    while (listView1.SelectedItems.Count > 0)
+                    {
+                        listView1.Items.Remove(listView1.SelectedItems[0]);
+                    }
+
+                    // 更新保存的路径
+                    SaveSoftwarePaths();
+                }
+            }
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
